881b84354d28dcf052e24b349acc4fc27666b6b0
KhacLong <vukhaclong03@gmail.com>
2024-08-15 23:47:20 +0700

    Second Commit

--- crawl_detail_data.py
+++ crawl_detail_data.py
@@ -11,6 +11,7 @@ website = "https://nhathuoclongchau.com.vn/"
 
 def main():
     p_id = 0
+    val = 387
 
     uris = [
         "thuc-pham-chuc-nang",
@@ -29,25 +30,29 @@ def main():
     time.sleep(3)
 
     for uri in uris:
-        data = {
-            "product_id": [],
-            "product_name": [],
-            "product_category": [],
-            "product_category_tree": [],
-            "usage": [],
-            "specification": [],
-            "description": [],
-            "image_urls": [],
-            "product_url": []
-        }
 
         df = pd.read_csv(f'./data/{uri}_product.csv')
         data_dicts = df.to_dict(orient='records')
 
         for data_dict in data_dicts:
+            if p_id < val:
+                p_id += 1
+                continue
+
+            data = {
+                "product_id": [],
+                "product_name": [],
+                "product_category": [],
+                "product_category_tree": [],
+                "usage": [],
+                "specification": [],
+                "description": [],
+                "image_urls": [],
+                "product_url": []
+            }
 
             driver.get(f"{data_dict['product_url']}")
-            time.sleep(1)
+            time.sleep(5)
 
             # process category and category tree
             category = []
@@ -59,40 +64,55 @@ def main():
                 if len(category) > 0:
                     category_tree += ">>"
                 category_tree += category[i]
+            if len(category) == 0:
+                print(f"Crawl num {p_id} failed!")
+                p_id += 1
+                continue
 
             # process usage
-            usage = driver.find_element(By.XPATH, "//div[@class='usage']/div/p").get_attribute("innerHTML")
+            usage = ""
+            try:
+                usage = driver.find_element(By.XPATH, "//div[@class='usage']/div/p").get_attribute("innerHTML")
+            except Exception:
+                pass
 
             specification = ""
             description = ""
 
-            for row in driver.find_elements(By.XPATH, "//table[@class='content-list']/tbody/tr"):
-                if row.find_element(By.XPATH, "./td[1]/p").get_attribute("innerHTML") == "Quy cách":
-                    specification = driver.find_element(By.XPATH, "//table[@class='content-list']/tbody/tr[4]/td[2]/div").get_attribute("innerHTML")
+            try:
+                for row in driver.find_elements(By.XPATH, "//table[@class='content-list']/tbody/tr"):
+                    if row.find_element(By.XPATH, "./td[1]/p").get_attribute("innerHTML") == "Quy cách":
+                        specification = driver.find_element(By.XPATH, "//table[@class='content-list']/tbody/tr[4]/td[2]/div").get_attribute("innerHTML")
 
-                if row.find_element(By.XPATH, "./td[1]/p").get_attribute("innerHTML") == "Mô tả ngắn":
-                    description = driver.find_element(By.XPATH, "//table[@class='content-list']/tbody/tr[8]/td[2]/div").get_attribute("innerHTML")
+                    if row.find_element(By.XPATH, "./td[1]/p").get_attribute("innerHTML") == "Mô tả ngắn":
+                        description = driver.find_element(By.XPATH, "//table[@class='content-list']/tbody/tr[8]/td[2]/div").get_attribute("innerHTML")
+            except Exception:
+                pass
 
             image_urls = ""
-            for row in driver.find_elements(By.XPATH, "//div[@class='lg-thumb-outer lg-thumb-align-middle lg-grab']/div/div"):
-                image_url = row.find_element(By.XPATH, "./img").get_attribute("src")
-                if len(image_url) > 0:
-                    image_urls += "***"
-                image_urls += image_url
+            try:
+                for row in driver.find_elements(By.XPATH, "//div[@class='lg-thumb-outer lg-thumb-align-middle lg-grab']/div/div"):
+                    image_url = row.find_element(By.XPATH, "./img").get_attribute("src")
+                    if len(image_url) > 0:
+                        image_urls += "***"
+                    image_urls += image_url
+            except Exception:
+                pass
 
-            data['product_id'] = p_id
+            data['product_id'].append(p_id)
+            data['product_name'].append(data_dict['product_name'])
+            data['product_category'].append(category[-1])
+            data['product_category_tree'].append(category_tree)
+            data['usage'].append(usage)
+            data['specification'].append(specification)
+            data['description'].append(description)
+            data['image_urls'].append(image_urls)
+            data['product_url'].append(data_dict['product_url'])
+
+            new_df = pd.DataFrame(data)
+            new_df.to_csv(f"./tmp/{uri}_product_{p_id}.csv", index=False)
+            print(f"Crawl num {p_id} successfully!")
             p_id += 1
-            data['product_name'] = data_dict['product_name']
-            data['product_category'] = category[-1]
-            data['product_category_tree'] = category_tree
-            data['usage'] = usage
-            data['specification'] = specification
-            data['description'] = description
-            data['image_urls'] = image_urls
-            data['product_url'] = data_dict['product_url']
-
-        new_df = pd.DataFrame(data)
-        new_df.to_csv(f"./detail_data/{uri}_product.csv", index=False)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -21,14 +21,26 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option("detach", True)
+# driver = webdriver.Chrome(options=options)
+#
+# website = "https://nhathuoclongchau.com.vn/trang-thiet-bi-y-te/urgo-scarform-gel-dieu-tri-seo-phi-dai-seo-loi-and-mo-tham-7-g.html"
+# # get all category_url
+# driver.get(website)
+# driver.maximize_window()
+# time.sleep(3)
+#
+# print(driver.find_element(By.XPATH, "//div[@class='usage']/div").get_attribute("innerHTML"))
 
-website = "https://nhathuoclongchau.com.vn/trang-thiet-bi-y-te/urgo-scarform-gel-dieu-tri-seo-phi-dai-seo-loi-and-mo-tham-7-g.html"
-# get all category_url
-driver.get(website)
-driver.maximize_window()
-time.sleep(3)
+for i in range(10):
+    num = []
+    try:
+        if i < 3:
+            num.append(i)
+        else:
+            raise Exception
+    except:
+        continue
+    print(i)
 
-print(driver.find_element(By.XPATH, "//div[@class='usage']/div").get_attribute("innerHTML"))
